a2fc4c747a595d35c3358a1f0376ec5af41104d8
Pongsakorn (Moo) <>
2023-09-18 17:50:25 +0700

    Init project

---
+++ src/main/kotlin/th/co/scb/sagapattern/SagaPatternApplication.kt
@@ -0,0 +1,11 @@
+package th.co.scb.sagapattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SagaPatternApplication
+
+fun main(args: Array<String>) {
+	runApplication<SagaPatternApplication>(*args)
+}
